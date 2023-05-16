5de1763fd52b69a699efd96788779eef035d4158
Katherine Vail <jabberduckygame@gmail.com>
2023-05-16 14:44:40 -0500

    Corrected grammar

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -7,7 +7,7 @@
             var answer = 4;
             var response = (answer < 9) ? "is less than nine" : "is greater than or equal to nine";
 
-            Console.WriteLine($"{answer} is {response}");
+            Console.WriteLine($"{answer} {response}");
         }
     }
 }
